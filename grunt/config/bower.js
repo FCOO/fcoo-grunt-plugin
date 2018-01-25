21e57d26ac8b296032f5f3659d247a6f5f917ddd
Niels Holt <niels@steenbuchholt.dk>
2018-01-25 19:00:22 +0100

    Removed check for existence when finding all font and images

--- grunt/config/bower.js
+++ grunt/config/bower.js
@@ -5,19 +5,19 @@
 module.exports = function ( grunt ) {
 
     var options = grunt.fcoo.options,
-        paths   = grunt.fcoo.paths;   
+        paths   = grunt.fcoo.paths;
 
-    
+
     return {
         //** bower: Copy all main-files from /bower_components to temp/. Only used to get all images and fonts into /temp **
         to_temp: {
-            base: paths.bower_components, 
+            base: paths.bower_components,
             dest: paths.temp,
             options: {
-                checkExistence: options.notDEBUG, //true=all bower components must be pressent. false=allows missing files (only in debug)
-                debugging     : options.DEBUG,    
+                checkExistence: false, //options.notDEBUG, //true=all bower components must be pressent. false=allows missing files
+                debugging     : options.DEBUG,
                 paths: {
-                    bowerDirectory: paths.bower_components, 
+                    bowerDirectory: paths.bower_components,
                     bowerrc       : '.bowerrc',
                     bowerJson     : 'bower.json'
                 }
