9d58df5895c22a71d93f198497d0c7c96a2041cb
Niels Holt <nho@fcoo.dk>
2019-05-02 11:04:02 +0200

    Bug fix in critical.js

--- grunt/config/critical.js
+++ grunt/config/critical.js
@@ -10,12 +10,11 @@ module.exports = function ( grunt ) {
     return {
         build: {
             options: {
-                maxImageFileSize: 100000,
-                css             : paths.temp_dist_APPLICATIONNAME_TIMPSTAMP_css,
-
-                inline          : true,             // true = Inline the generated critical-path CSS - true generates HTML - false generates CSS
-                extract         : true,             // true = Extract inlined styles from referenced stylesheets
-                minify          : options.notDEBUG, // true = Minify critical-path CSS when inlining
+                base   : paths.temp_dist,
+                css    : paths.temp_dist_APPLICATIONNAME_TIMPSTAMP_css,
+                inline : true,             // true = Inline the generated critical-path CSS - true generates HTML - false generates CSS
+                extract: true,             // true = Extract inlined styles from referenced stylesheets
+                minify : options.notDEBUG, // true = Minify critical-path CSS when inlining
             },
             src : paths.temp_dist + 'index.html',
             dest: paths.temp_dist + 'index.html'
