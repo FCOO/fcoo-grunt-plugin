dd74d12124e6594c4f0b59561059817e69969ad3
Niels Holt <niels@steenbuchholt.dk>
2020-09-29 13:12:51 +0200

    Clean up

--- grunt/config/babel.js
+++ grunt/config/babel.js
@@ -22,10 +22,5 @@ module.exports = function ( grunt ) {
     //Not the right way to do it, but it works! :-)
     result.dist.files[paths.temp_dist_APPLICATIONNAME_TIMPSTAMP_js] = paths.temp_dist_APPLICATIONNAME_TIMPSTAMP_js;
 
-//HER    result.babel.dist.files[paths.temp_dist_APPLICATIONNAME_TIMPSTAMP_js] = paths.temp_dist_APPLICATIONNAME_TIMPSTAMP_js;
-
-//    result.babel.test.dist.files[paths.temp_dist_APPLICATIONNAME_TIMPSTAMP_js+'NOT'] = 'temp_dist/niels.js';
-//    result.babel.dist.files[paths.temp_dist_APPLICATIONNAME_TIMPSTAMP_js+'NOT'] = 'temp_dist/niels.js';
-console.log(result);
     return result;
 }
